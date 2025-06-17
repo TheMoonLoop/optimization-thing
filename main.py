a493e208e779caf4e5933c0147055a0a098bac2e
Ren <110518475+TheMoonLoop@users.noreply.github.com>
2025-06-17 09:24:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import streamlit as st
 with st.sidebar:
   st.header("hola")
   st.page_link("pages/prueba.py", label="penis test")
-  st.page_link("pages/EINAR es un pendejo.py", label="second penis test")
+  st.page_link("pages/EINAR es un Pendejo.py", label="second penis test")
